12e42b2df1186b668b6302912110de8f235bc1ab
Ashish Agrawal <ashish8754@gmal.com>
2023-04-22 12:56:57 +0530

    adding insert search and delete operation to LL

--- src/main/java/Basics/LinkedListDemo.java
+++ src/main/java/Basics/LinkedListDemo.java
@@ -8,9 +8,25 @@ public class LinkedListDemo {
         System.out.println(Sll.isEmpty());
         for(int i=0;i<10;i++)
         {
-            Sll.insertAtHead(i);
+            Sll.insertAtEnd(i);
             Sll.printList();
         }
+        Sll.printList();
+        System.out.println(Sll.size);
+
+
+//        SinglylinkedList Sll2=new SinglylinkedList();
+//        System.out.println(Sll2.isEmpty());
+//        for(int i=0;i<10;i++)
+//        {
+//            Sll2.insertAtHead(i);
+//            Sll2.printList();
+//        }
+//        System.out.println(Sll2.size);
+
+        Sll.insertAfter(20,5);
+        Sll.printList();
+        System.out.println(Sll.searchNode(20));
 
     }
 }
--- src/main/java/Basics/SinglylinkedList.java
+++ src/main/java/Basics/SinglylinkedList.java
@@ -18,6 +18,25 @@ public class SinglylinkedList {
         return headNode == null;
     }
 
+    boolean searchNode(int data)
+    {
+        if(isEmpty())
+        {
+            System.out.println("List is Empty");
+            return false;
+        }
+
+        Node current=new Node();
+        current=this.headNode;
+        while(current.nextNode!=null)
+        {
+            if(current.data==data)
+                return true;
+
+            current=current.nextNode;
+        }
+        return false;
+    }
     public void insertAtHead(int data)
     {
         Node newNode=new Node();
@@ -27,6 +46,52 @@ public class SinglylinkedList {
         headNode=newNode;
         size++;
     }
+
+    public void insertAtEnd(int data)
+    {
+        if(isEmpty())
+        {
+            insertAtHead(data);
+        }
+        else
+        {
+            Node n=new Node();
+            Node temp=new Node();
+            temp=headNode;
+            n.data=data;
+            while(temp.nextNode!=null)
+            {
+                temp=temp.nextNode;
+            }
+            temp.nextNode=n;
+            n.nextNode=null;
+            size++;
+
+        }
+    }
+
+    void insertAfter(int data, int previous)
+    {
+            Node n=new Node();
+            Node current=new Node();
+            current=this.headNode;
+            n.data=data;
+            while(current != null && current.data!=previous)
+            {
+                current=current.nextNode;
+            }
+            if(current != null)
+            {
+                n.nextNode=current.nextNode;
+                current.nextNode=n;
+                size++;
+            }
+
+
+
+    }
+
+
 
     public void printList()
     {
