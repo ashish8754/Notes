701bd9813c7c2b8eeb6528bed4660e92c45a3034
Ashish Agrawal <ashish8754@gmal.com>
2023-05-04 13:35:00 +0530

    separating educative questions package

--- src/main/java/EducativeQuestions/Main.java
+++ src/main/java/EducativeQuestions/Main.java
@@ -2,20 +2,27 @@ package EducativeQuestions;
 
 public class Main {
     public static void main(String[] args) {
-        QueueUsingStack<Integer> queue = new QueueUsingStack<>(5);
+//        QueueUsingStack<Integer> queue = new QueueUsingStack<>(5);
+//
+//        queue.enqueue(1);
+//        queue.enqueue(2);
+//        queue.enqueue(3);
+//        queue.enqueue(4);
+//        queue.enqueue(5);
+//
+//        System.out.println(queue.dequeue());
+//        System.out.println(queue.dequeue());
+//        System.out.println(queue.dequeue());
+//        System.out.println(queue.dequeue());
+//        System.out.println(queue.dequeue());
+//        System.out.println(queue.dequeue()); //this will output null because queue will be empty
 
-        queue.enqueue(1);
-        queue.enqueue(2);
-        queue.enqueue(3);
-        queue.enqueue(4);
-        queue.enqueue(5);
-
-        System.out.println(queue.dequeue());
-        System.out.println(queue.dequeue());
-        System.out.println(queue.dequeue());
-        System.out.println(queue.dequeue());
-        System.out.println(queue.dequeue());
-        System.out.println(queue.dequeue()); //this will output null because queue will be empty
+        Graph bfstest=new Graph(5);
+        bfstest.addEdge(0,1);
+        bfstest.addEdge(0,2);
+        bfstest.addEdge(1,3);
+        bfstest.addEdge(1,4);
+        bfstest.printGraph();
 
 
 
---
+++ src/main/java/EducativeQuestions/checkBFS.java
@@ -0,0 +1,11 @@
+package EducativeQuestions;
+
+public class checkBFS {
+
+
+
+    public String bfs(Graph g)
+    {
+        return null;
+    }
+}
